2969b78d18ed4a1892d7ac3fbd892bb13b54154b
fejdraus <fejdraus@gmail.com>
2023-06-28 13:19:51 +0300

    Add mock for testing rules

---
+++ ConsoleMock/Program.cs
@@ -0,0 +1,19 @@
+﻿// Путь к файлу
+string path = "test.txt";
+
+try
+{
+    // Чтение всех строк файла
+    string[] lines = File.ReadAllLines(path);
+
+    // Построчный вывод содержимого файла
+    foreach (string line in lines)
+    {
+        Console.WriteLine(line);
+    }
+}
+catch (Exception ex)
+{
+    // В случае ошибки выводим информацию об ошибке
+    Console.WriteLine($"Не удалось прочитать файл: {ex.Message}");
+}
